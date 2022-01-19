a8e1f57ac54fe76ff0927c479029ccec7a040f90
yuanhua.zhu <zhuyuanhua@aliyun.com>
2022-01-19 12:29:35 +0800

    second commit

--- src/main/java/com/bayhwa/git/GitTest.java
+++ src/main/java/com/bayhwa/git/GitTest.java
@@ -2,6 +2,7 @@ package com.bayhwa.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.print("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git2");
     }
 }
